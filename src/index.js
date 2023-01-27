9c3ba0113fd637fe6d7aba1b2aaf0a32ee05f14e
unknown <s.ozinkovskyi@gmail.com>
2023-01-27 14:29:20 +0200

    feat: add methods toSring(), split(), reverse(), join(), parseInt()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let num = n < 0 ? Math.abs(n) : n;
+
+    let strNum = num.toString().split("").reverse().join("");
+    let result = parseInt(strNum);
+
+    return result;
+};
